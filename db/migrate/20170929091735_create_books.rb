567beb43769d646683fbd722b2466efcd0eb2dcf
Valerii Kuzin <kuzin.valeriy@gmail.com>
2017-09-29 12:38:07 +0300

    Added scaffold Book

---
+++ db/migrate/20170929091735_create_books.rb
@@ -0,0 +1,13 @@
+class CreateBooks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.integer :user_id
+      t.text :image
+      t.text :summary
+      t.boolean :draft, default: false
+
+      t.timestamps
+    end
+  end
+end

9ce47b70570c52700e06bc1892a8a9d1c6174999
Valerii Kuzin <kuzin.valeriy@gmail.com>
2017-09-30 23:50:23 +0300

    Implemented rails_admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def current_admin?
+    return true if user_signed_in? && current_user.admin?
+    redirect_to root_path
+  end
+
   protected
 
   def configure_permitted_parameters

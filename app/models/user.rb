3cf6e6c5893ee91664608321531b39235fbe57bf
Valerii Kuzin <kuzin.valeriy@gmail.com>
2017-10-01 15:48:35 +0300

    Added validation and tests for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,33 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name, presence: true, length: { in: 6..50 }
+
+  validates :password,
+            presence: true,
+            confirmation: true,
+            length: { in: 6..32 },
+            if: -> (m) { !m.password.nil? }
+
+  validates :email,
+            presence: true,
+            length: { in: 6..250 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+
   before_destroy :admin_protection
+  before_save   :downcase_email
 
   private
 
   def admin_protection
     throw(:abort) if self.admin?
+  end
+
+  def downcase_email
+    self.email = email.downcase
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,5 +12,77 @@ describe User do
     expect(user).to be_valid
   end
 
+  # name validation
+  it 'name should not be blank' do
+    user.name = ''
+    expect(user).to be_invalid
+  end
+
+  it 'name should not be short' do
+    user.name = 'kl'
+    expect(user).to be_invalid
+  end
+
+  it 'name should not be too long' do
+    user.name = 'k' * 51
+    expect(user).to be_invalid
+  end
+
+  # email validation
+  it "email is validated" do
+    expect(user).to validate_presence_of(:email)
+  end
+
+  it "email should be present" do
+    user.email = "     "
+    expect(user).to be_invalid
+  end
+
+  it "email should not be too long" do
+    user.email = "a" * 250  + "@example.com"
+    expect(user).to be_invalid
+  end
+
+  it 'email addresses should be unique' do
+    duplicate_user = user.dup
+    user.save
+    expect(duplicate_user).to be_invalid
+  end
+
+  it 'email addresses should be saved as lower-case' do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    user.email = mixed_case_email
+    user.save
+    expect(mixed_case_email.downcase).to eq(user.reload.email)
+  end
+
+  # password validate
+
+  it 'password should not be blank' do
+    user.name = ''
+    expect(user).to be_invalid
+  end
+
+  it 'password should not be short' do
+    user.password = 'kl'
+    expect(user).to be_invalid
+  end
+
+  it 'password should not be too long' do
+    user.password = 'k' * 33
+    expect(user).to be_invalid
+  end
+
+  it 'password should be like password_confirmed' do
+    user.password = '2222222'
+    expect(user).to be_invalid
+  end
+
+  # admin
+  it 'admin_user should not be destroy' do
+    user.admin = true
+    user.destroy
+    expect(user).to be_valid
+  end
 
 end

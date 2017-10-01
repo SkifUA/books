241528d3e93e1ba97f90d22b87c0e214f683ada7
Valerii Kuzin <kuzin.valeriy@gmail.com>
2017-10-01 21:39:44 +0300

    Updated create book test

--- spec/features/create_book_spec.rb
+++ spec/features/create_book_spec.rb
@@ -14,13 +14,24 @@ feature "Login" do
     click_button "Log in"
 
     page.find('a', text: 'New Book').click
+    expect(page).to have_css( 'form', id:'form-book')
 
     fill_in "Title", with: "Book created by test"
     fill_in "Author", with: "Rspec Test"
     fill_in "Summary", with: "Book created by Rspec Test"
     click_button "Save Book"
 
+    expect(page).not_to have_text('Save Book')
     expect(page).to have_text('Book created by test')
+  end
+
+  scenario "User can't create New Book" do
+
+    visit "/"
+
+    page.find('a', text: 'New Book').click
+
+    expect(page).not_to have_css( 'form', id:'form-book')
   end
 
 end

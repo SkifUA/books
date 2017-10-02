117dc19707edeae9fcb5f755197c8a3e84684011
Valerii Kuzin <kuzin.valeriy@gmail.com>
2017-10-02 23:13:49 +0300

    Added cascad to user for delete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :books
+  has_many :books, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
